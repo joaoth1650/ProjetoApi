de92ee34f21dd3b470002b828568c294fea8a7e9
joaoth <joaopedromcj9@gmail.com>
2023-05-03 17:33:02 -0300

    next back

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const btnGo = document.getElementById('btn-go');
 const btnReset = document.getElementById('btn-reset');
 const btnAll =document.getElementById('btn-all');
 const content = document.getElementById('content');
+
 const conteinerResult = document.getElementById('result-style');
 const image = document.getElementById('img');
 const anterior = document.getElementById("anterior");
@@ -30,6 +31,8 @@ const fetchApiEpisode = (url) => {
   return result;
 }
 
+
+
 const keys = ['name', 'status', 'species', 'gender', 'origin', 'episode'];
 const newKeys = {
   name: 'Nome',
@@ -48,15 +51,21 @@ const newKeys = {
 //   console.log(url)
 // }
 
-
-
-const funcao_clicou_no_botao = async (url) => {
+const funcao_clicou_no_botao = async (url, next=undefined, back=undefined) => {
   let result = await fetchApiEpisode(url)
-  console.log(url)
   const personag = result.characters.join('\r\n\n')
+  console.log({next}, {back})
+  if(next === undefined && back === undefined){
+    console.log({linkEpisodios})
+    let
+    // quem é o proximo ou anterior
+    next = `${proximo}`;
+    back = `${anterior}`;
+  }
+  
   Swal.fire({
-    html: `<div class="px-4 nav-sweet"><img src="RMapi.png"></div>
-           <div class="p-4 bg-do-sweet"><h2> Name: ${result.name}</h2>
+    html: `<div class="px-4 m-4 nav-sweet"><img src="RMapi.png"></div>
+           <div class="p-4 bg-do-sweet" id="sweet-content"><h2> Name: ${result.name}</h2>
            <h2> Personagens: ${personag}</h2>
            <h2> Lançamento:${result.air_date}</h2>
            <h2> Temporada: ${result.episode}</h2>
@@ -65,30 +74,17 @@ const funcao_clicou_no_botao = async (url) => {
            
            <img src="thumb.jpg" class="m-2"style="width:400px; height="130px">
            </div>
-           <div class="footer-sweet">
-           <span class="btn btn-primary" id="proximo">Próximo Episódio</span>
-           </div>
-           <span class="btn btn-primary" id="anterior">Episódio Anterior</span>
+           <div class="footer-sweet row">
+           <span class="btn btn-primary col-2 d-grid gap-2 d-md-block" id="anterior" onclick="funcao_clicou_no_botao('${back}')">Episódio Anterior</span>
+           <div class="col-8"></div>
+           <span class="btn btn-primary col-2 d-md-flex justify-content-md-end" id="proximo" onclick="funcao_clicou_no_botao('${next}')">Próximo Episódio</span>
+           
            </div>`,
     width:'50%',
   })
-//     const proximaURL = (url) => {
-//       url.split('')
-//       let urlNum = parseInt(url[40] + url[41])
-//       let copcop = urlNum + 1
-//       url = `https://rickandmortyapi.com/api/episode/${copcop}`
-//       console.log(url)
-// }
 }
 
-// proximo.addEventListener('click', (event) => {
-//   event.preventDefault();
-//    if (1 = 1) 
-//    return url = `${proximaURL}`
-// })
-
-
-
+const linkEpisodios = [];
 
 const buildResult = (result) => {
   return keys.map((key) => document.getElementById(key))
@@ -97,14 +93,17 @@ const buildResult = (result) => {
     content2.innerHTML = ""
     if(elem.checked == true && (Array.isArray(result[elem.name])) == true){
       const arrayResult = result[elem.name];
-      arrayResult.map((re) => {
+      linkEpisodios.push(...arrayResult)
+      arrayResult.map((re,index) => {
         const newElem = document.createElement('span');
         const letra = re.replace('https://rickandmortyapi.com/api/episode/', 'ep ');
         newElem.className = 'btn btn-primary m-3 p-3 rounded-circle p-3';
         newElem.innerHTML = `${letra}`; //replace
         newElem.dataset.link = re;
+        let next = arrayResult[index + 1]
+        let back = arrayResult[index - 1]
         newElem.onclick = function() { 
-          funcao_clicou_no_botao(re)
+          funcao_clicou_no_botao(re, next, back)
         };
         content2.appendChild(newElem);
       });
