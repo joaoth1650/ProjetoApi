b4c0bc890c5a0f015205feb5a513dce8c2971016
joaoth <joaopedromcj9@gmail.com>
2023-05-12 17:28:39 -0300

    imagina ter que deixar isso responsivo

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const characterName= document.getElementById('characterName');
+const characterName = document.getElementById('characterName');
 const btnGo = document.getElementById('btn-go');
 const btnReset = document.getElementById('btn-reset');
-const btnAll =document.getElementById('btn-all');
+const btnAll = document.getElementById('btn-all');
 const content = document.getElementById('content');
 const conteinerResult = document.getElementById('result-style');
 const image = document.getElementById('img');
@@ -9,79 +9,95 @@ const aondeImagensFicam = document.getElementById('aondeImagensFicam');
 const anterior = document.getElementById("anteriorButton");
 const proximoButton = document.getElementById("proximoButton");
 
-
+//somente fetch abaixo //
 
 const fetchApiNameCharacter = (value) => {
-  const result = fetch(`https://rickandmortyapi.com/api/character/?name=${value}`)
-  .then((res) => res.json()) // res = response/ responder em json
-  .then((data) => {
-    console.log(data); // data = dado
-    return data;
-  });
+    const result = fetch(`https://rickandmortyapi.com/api/character/?name=${value}`)
+        .then((res) => res.json()) // res = response/ responder em json
+        .then((data) => {
+            console.log(data); // data = dado
+            return data;
+        });
 
-  return result;
+    return result;
 }
 
+const fetchApi = (value) => {
+    const result = fetch(`https://rickandmortyapi.com/api/character/${value}`)
+        .then((res) => res.json()) // res = response/ responder em json
+        .then((data) => {
+            console.log(data); // data = dado
+            return data;
+        });
+
+    return result;
+}
 
 const fetchApiEpisode = (url) => {
-  const result = fetch(`${url}`)
-  .then((res) => res.json()) // res = response/ responder em json
-  .then((data) => {
-    console.log(data); // data = dado
-    return data;
-  });
+    const result = fetch(`${url}`)
+        .then((res) => res.json()) // res = response/ responder em json
+        .then((data) => {
+            console.log(data); // data = dado
+            return data;
+        });
 
-  return result;
+    return result;
 }
-
-
 
 const fetchApiAllCharacters = () => {
-  const result = fetch(`https://rickandmortyapi.com/api/character/${generateRandomInteger(800)},${generateRandomInteger(800)},${generateRandomInteger(800)},${generateRandomInteger(800)},${generateRandomInteger(800)},${generateRandomInteger(800)},${generateRandomInteger(800)},${generateRandomInteger(800)},${generateRandomInteger(800)},${generateRandomInteger(800)},${generateRandomInteger(800)},${generateRandomInteger(800)},${generateRandomInteger(800)},${generateRandomInteger(800)},${generateRandomInteger(800)},${generateRandomInteger(800)},${generateRandomInteger(800)},${generateRandomInteger(800)},${generateRandomInteger(800)},${generateRandomInteger(800)},${generateRandomInteger(800)},${generateRandomInteger(800)},${generateRandomInteger(800)},${generateRandomInteger(800)}`)
-  .then((res) => res.json()) // res = response/ responder em json
-  .then((data) => {
-    console.log(data); // data = dado
-    return data;
-  });
+    const result = fetch(`https://rickandmortyapi.com/api/character/${generateRandomInteger(800)},${generateRandomInteger(800)},${generateRandomInteger(800)},${generateRandomInteger(800)},${generateRandomInteger(800)},${generateRandomInteger(800)},${generateRandomInteger(800)},${generateRandomInteger(800)},${generateRandomInteger(800)},${generateRandomInteger(800)},${generateRandomInteger(800)},${generateRandomInteger(800)},${generateRandomInteger(800)},${generateRandomInteger(800)},${generateRandomInteger(800)},${generateRandomInteger(800)},${generateRandomInteger(800)},${generateRandomInteger(800)},${generateRandomInteger(800)},${generateRandomInteger(800)},${generateRandomInteger(800)},${generateRandomInteger(800)},${generateRandomInteger(800)},${generateRandomInteger(800)}`)
+        .then((res) => res.json()) // res = response/ responder em json
+        .then((data) => {
+            console.log(data); // data = dado
+            return data;
+        });
 
-  return result;
+    return result;
 }
 
+// somente fetch acima //
+
+// inclementos abaixo //
 function generateRandomInteger(max) {
-  return Math.floor(Math.random() * max) + 1;
+    return Math.floor(Math.random() * max) + 1;
+}
+
+const consultaPerson = async (id) => {
+    const result = await fetchApi(id)
+    buildResult(result)
 }
 
 const keys = ['name', 'status', 'species', 'gender', 'origin', 'episode'];
 const newKeys = {
-  name: 'Nome',
-  status: 'Status',
-  species: 'Espécie',
-  gender: 'Gênero',
-  origin: 'Origem',
-  episode: 'Episódios',
+    name: 'Nome',
+    status: 'Status',
+    species: 'Espécie',
+    gender: 'Gênero',
+    origin: 'Origem',
+    episode: 'Episódios',
 }
 
+// inclementos acima //
 
+const funcao_clicou_no_botao = async (url, next = undefined, back = undefined) => {
+    let result = await fetchApiEpisode(url)
+    const personag = result.characters.join('\r\n')
+    console.log({ next }, { back })
+    if (next === undefined && back === undefined) {
+        console.log({ linkEpisodios })
 
-const funcao_clicou_no_botao = async (url, next=undefined, back=undefined) => {
-  let result = await fetchApiEpisode(url)
-  const personag = result.characters.join('\r\n')
-  console.log({next}, {back})
-  if(next === undefined && back === undefined){
-    console.log({linkEpisodios})
-
-    for (let i = 0; i < linkEpisodios.length; i++) {
-      if (linkEpisodios[i] === url) {
-        // proximo.disabled = true;
-        next = linkEpisodios[i + 1]
-        back = linkEpisodios[i - 1]
-      }
+        for (let i = 0; i < linkEpisodios.length; i++) {
+            if (linkEpisodios[i] === url) {
+                // proximo.disabled = true;
+                next = linkEpisodios[i + 1]
+                back = linkEpisodios[i - 1]
+            }
+        }
     }
-  }
-  let meuHtml = `<div class="px-4 m-4 nav-sweet"><img src="RMapi.png"></div>`;
-  
-  Swal.fire({
-    html: `${meuHtml}
+    let meuHtml = `<div class="px-4 m-4 nav-sweet"><img src="img/RMapi.png"></div>`;
+
+    Swal.fire({
+        html: `${meuHtml}
            <div class="p-4 bg-do-sweet" id="sweet-content"><h2> Name: ${result.name}</h2>
            <h2> Personagens: ${personag}</h2>
            <h2> Lançamento:${result.air_date}</h2>
@@ -97,130 +113,172 @@ const funcao_clicou_no_botao = async (url, next=undefined, back=undefined) => {
            <span class="btn btn-primary col-3" id="proximoButton" onclick="funcao_clicou_no_botao('${next}')">Próximo Episódio</span>
            
            </div>`,
-    width:'55%',
-    height:'30%',
+        width: '55%',
+        height: '30%',
 
-  })
-   if ( next===undefined ) {
-    const proximoButton = document.getElementById('proximoButton');
-    proximoButton.classList.add('sumiu')
-    const anterior = document.getElementById('anteriorButton');
-    anterior.classList.remove('col-3')
-  } else if (back===undefined) {
-    const anterior = document.getElementById('anteriorButton');
-    anterior.classList.add('sumiu')
-    const proximoButton = document.getElementById('proximoButton');
-    proximoButton.classList.remove('col-3')
-    
-    
-  }
+    })
+    if (next === undefined) {
+        const proximoButton = document.getElementById('proximoButton');
+        proximoButton.classList.add('sumiu')
+        const anterior = document.getElementById('anteriorButton');
+        anterior.classList.remove('col-3')
+    } else if (back === undefined) {
+        const anterior = document.getElementById('anteriorButton');
+        anterior.classList.add('sumiu')
+        const proximoButton = document.getElementById('proximoButton');
+        proximoButton.classList.remove('col-3')
+
+
+    }
 }
 
 
+// Construção da pagina com DOM //
 
 const buildPesquisa = async () => {
-  let opçoesCharacters = await fetchApiNameCharacter(characterName.value);
- aondeImagensFicam.innerHTML =""
-  opçoesCharacters.results.map((result) => {
-    const personagensResult = result.image
-    let conteudo = `
+    let opçoesCharacters = await fetchApiNameCharacter(characterName.value);
+
+    aondeImagensFicam.innerHTML = ""
+
+    opçoesCharacters.results.map((result) => {
+        const personagensResult = result.image
+        const nomeDosPersonagens = result.name
+        const idPersonagem = result.id
+        const episodes = result.episode
+        let conteudo = ""
+        conteudo = `
       <div class= "col-lg-2">
       <img src="${personagensResult}" class="p-4 img-fluid">
+      <p><span onclick="consultaPerson(${idPersonagem})">${nomeDosPersonagens}</span></p>
       </div>
     `;
-    aondeImagensFicam.innerHTML = aondeImagensFicam.innerHTML + conteudo;
-    
-   
-  })
+
+        aondeImagensFicam.innerHTML = aondeImagensFicam.innerHTML + conteudo;
+
+    })
 }
 
-// buildPesquisa()
-
-const linkEpisodios = [];
 const buildResult = (result) => {
-  return keys.map((key) => document.getElementById(key))
-  .map((elem) => {
-    const content2 = document.getElementById('eps');
-    content2.innerHTML = ""
-    if(elem.checked == true && (Array.isArray(result[elem.name])) == true){
-      const arrayResult = result[elem.name];
-      linkEpisodios.push(...arrayResult)
-      arrayResult.map((url,index) => {
-        const newElem = document.createElement('span');
-        const letra = url.replace('https://rickandmortyapi.com/api/episode/', 'ep ');
-        newElem.className = 'btn btn-primary m-3 p-3 rounded-circle p-3';
-        newElem.innerHTML = `${letra}`; //replace
-        newElem.dataset.link = url;
-        let next = arrayResult[index + 1]
-        let back = arrayResult[index - 1]
-        newElem.onclick = function() { 
-          funcao_clicou_no_botao(url, next, back)
-        };
-        content2.appendChild(newElem);
-      });
-      } else if(elem.checked == true && (elem.name == 'origin')){ // se checar que foi selecionado o elemento 'origin'
-        const newElem = document.createElement('p');
-        newElem.innerHTML = `${newKeys[elem.name]}: ${result[elem.name].name}`; 
-        content.appendChild(newElem);
-      } else if(elem.checked == true && typeof(result[elem.name]) !== 'object'){ // typeof para vereficar o tipo do resultado que esta sendo entregue para nao ter que determinar cada chave 
-        const newElem = document.createElement('p');
-        newElem.innerHTML = `${newKeys[elem.name]}: ${result[elem.name]}`;
-        content.appendChild(newElem); 
-      } 
-    });
+
+    const origin = result.origin;
+    const episodes = result.episode;
+
+    let conteudo = ``
+    conteudo = `
+        <div class="container row">
+        <div class="col">
+           <div>
+           <img src="${result.image}" id="personImage">
+           </div>
+                <h1 class="">${result.name}</h1>
+                <div class="row">
+                 
+                 <h3 class="col-12">status: ${result.status}</h3>
+                 <h3 class="col-12">especie: ${result.species}</h3>
+                 <h3 class="col-12">genero: ${result.gender}</h3>
+                 <h3 class="col-12"origem: >${origin.name}</h3>
+               </div>
+            <div class="row">
+              <div class="col">
+              <select class="form-select col" >
+                ${episodes.map(episode => `<option value="${episode}">${episode}</option>`).join('')}
+                </select>
+              <span class="btn btn-primary col" onchange="funcao_clicou_no_botao(this.value)">VAI</span> 
+              </div>
+            </div>
+        </div>
+        </div>
+     `
+    conteinerResult.innerHTML = conteudo
 }
+const linkEpisodios = [];
+// return keys.map((key) => document.getElementById(key))
+// 	.map((elem) => {
+// 		const content2 = document.getElementById('eps');
+// 		content2.innerHTML = ""
+// 		if (elem.checked == true && (Array.isArray(result[elem.name])) == true) {
+// 			const arrayResult = result[elem.name];
+// 			linkEpisodios.push(...arrayResult)
+// 			arrayResult.map((url, index) => {
+// 				const newElem = document.createElement('span');
+// 				const letra = url.replace('https://rickandmortyapi.com/api/episode/', 'ep ');
+// 				newElem.className = 'btn btn-primary m-3 p-3 rounded-circle p-3';
+// 				newElem.innerHTML = `${letra}`; //replace
+// 				newElem.dataset.link = url;
+// 				let next = arrayResult[index + 1]
+// 				let back = arrayResult[index - 1]
+// 				newElem.onclick = function () {
+// 					funcao_clicou_no_botao(url, next, back)
+// 				};
+// 				content2.appendChild(newElem);
+// 			});
+// 		}
+// 		//    else if(elem.checked == true && (elem.name == 'origin')){ // se checar que foi selecionado o elemento 'origin'
+// 		//     const newElem = document.createElement('p');
+// 		//     newElem.innerHTML = `${newKeys[elem.name]}: ${result[elem.name].name}`; 
+// 		//     content.appendChild(newElem);
+// 		//   } else if(elem.checked == true && typeof(result[elem.name]) !== 'object'){ // typeof para vereficar o tipo do resultado que esta sendo entregue para nao ter que determinar cada chave 
+// 		//     const newElem = document.createElement('p');
+// 		//     newElem.innerHTML = `${newKeys[elem.name]}: ${result[elem.name]}`;
+// 		//     content.appendChild(newElem); 
+// 		//   } 
+// 		// });
+// 	}
+// 	)
 
 const BuildHome = async () => {
-  let personagens = await fetchApiAllCharacters();
+    let personagens = await fetchApiAllCharacters();
 
-  personagens.map((personagem) => {
-    const personagemImg = personagem.image
-    let conteudo = `
+    personagens.map((personagem) => {
+        const personagemImg = personagem.image
+        let conteudo = `
       <div class= "col-lg-2">
       <img src="${personagemImg}" class="p-4 img-fluid">
       </div>
     `;
-    aondeImagensFicam.innerHTML = aondeImagensFicam.innerHTML + conteudo;
-    // aondeImagensFicam.insertAdjacentHTML('afterEnd', conteudo);
-    // newElem.setAttribute("src", `${personagemImg}`);
-    // aondeImagensFicam.appendChild(newElem)
-  })
+        aondeImagensFicam.innerHTML = aondeImagensFicam.innerHTML + conteudo;
+        // aondeImagensFicam.insertAdjacentHTML('afterEnd', conteudo);
+        // newElem.setAttribute("src", `${personagemImg}`);
+        // aondeImagensFicam.appendChild(newElem)
+    })
 }
 
-// BuildHome()
+// Construção da pagina com DOM //
 
 
+// addEvent click abaixo //
 
 btnGo.addEventListener('click', async (event) => { //usando async para deixar assíncrono e ser entendido em tempo diferente do demais codigo  
-  event.preventDefault(); // preservar os dados na tela 
-  
-  if(characterName.value === ''){
-    return content.innerHTML = 'É necessário fazer um filtro.';
-  }
-  const result = await fetchApiNameCharacter(characterName.value); //await para ficar de acordo com o assincronismo  
+    event.preventDefault(); // preservar os dados na tela 
 
-  if(content.firstChild === null){ // primeiroFilho 
-    conteinerResult.className = 'result-style';
-    image.src = `${result.image}`; // adicionando uma classname ao pai de content e ajustando no css
-    buildPesquisa(result);
-  } else {
-    content.innerHTML = '';
-    conteinerResult.className = 'result-style';
-    image.src = `${result.image}`;
-    buildPesquisa(result);
-  }
-});
-
-btnReset.addEventListener('click', () => location.reload()); // reflash na pagina usando botao limpar (location.reload())
-
-btnAll.addEventListener('click', (event) => {
-    event.preventDefault();
-    for(let __key of keys){
-    checkboxes = document.getElementsByName(__key);
-    for(var checkbox of checkboxes){
-      checkbox.checked = true;
+    if (characterName.value === '') {
+        return content.innerHTML = 'É necessário fazer um filtro.';
     }
-  }
+    const result = await fetchApiNameCharacter(characterName.value); //await para ficar de acordo com o assincronismo  
+
+    if (content.firstChild === null) { // primeiroFilho 
+        conteinerResult.className = 'result-style';
+        image.src = `${result.image}`; // adicionando uma classname ao pai de content e ajustando no css
+        buildPesquisa(result);
+    } else {
+        content.innerHTML = '';
+        conteinerResult.className = 'result-style';
+        image.src = `${result.image}`;
+        buildPesquisa(result);
+    }
 });
+
+// btnReset.addEventListener('click', () => location.reload());
+// reflash na pagina usando botao limpar (location.reload())
+
+// btnAll.addEventListener('click', (event) => {
+//     event.preventDefault();
+//     for(let __key of keys){
+//     checkboxes = document.getElementsByName(__key);
+//     for(var checkbox of checkboxes){
+//       checkbox.checked = true;
+//     }
+//   }
+// });
 // const buttons = document.getElementBytagName("button"); buttons('eps');
 // buttons.className = 'btn btn-primary';
